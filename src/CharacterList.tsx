b7236c0d706df0618a1e56835194be4784700447
pocari <caffelattenonsugar@gmail.com>
2020-03-21 17:39:52 +0900

    semantic-ui入れてサンプル実装

---
+++ src/CharacterList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Header, Icon, Item } from 'semantic-ui-react';
+
+export interface Character {
+  id: number;
+  name: string;
+  age: number;
+  height?: number;
+}
+
+interface CharacterListPorps {
+  school: string;
+  characters: Character[];
+}
+
+const CharacterList: React.FC<CharacterListPorps> = props => {
+  const { school, characters } = props;
+
+  return (
+    <>
+      <Header as="h2">{school}</Header>
+      <Item.Group>
+        {characters.map(c => (
+          <Item>
+            <Icon name="user circle" size="huge" />
+            <Item.Content>
+              <Item.Header>{c.name}</Item.Header>
+              <Item.Meta>{c.age}歳</Item.Meta>
+              <Item.Meta>
+                {c.height ? c.height : '???'}
+                cm
+              </Item.Meta>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </>
+  );
+};
+export default CharacterList;
