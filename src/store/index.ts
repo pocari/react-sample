605a59e45c1de0dc3ee6654b0120cad4f8ebba32
pocari <caffelattenonsugar@gmail.com>
2020-03-28 14:10:00 +0900

    refactor reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux'
-import counterReducer from 'reducer'
+import counterReducer from 'reducers/counter'
 
 const rootReducer = combineReducers({
   counter: counterReducer
